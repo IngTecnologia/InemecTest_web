3dc25d92a7a1345c82f216d3cdf917c93a4ec5f9
jesuscotca <ing.tecnologia2@inemec.com>
2025-06-06 16:15:37 -0500

    BrowserRouter added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import EvaluationForm from '../components/evaluation/EvaluationForm.jsx'
+import AdminPage from './pages/AdminPage.jsx'
 
 function App() {
   return (
-    <div className="container">
-      <div className="header">
-        <h1>InemecTest</h1>
-        <p>Evaluación de Conocimientos Técnicos: Programa de Disciplina Operativa</p>
-      </div>
-      <EvaluationForm />
-    </div>
+    <Routes>
+      <Route path="/" element={
+        <div className="container">
+          <div className="header">
+            <h1>InemecTest</h1>
+            <p>Evaluación de Conocimientos Técnicos: Programa de Disciplina Operativa</p>
+          </div>
+          <EvaluationForm />
+        </div>
+      } />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
   )
 }
 
